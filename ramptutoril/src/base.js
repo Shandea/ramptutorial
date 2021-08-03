60767546efac2cd36e51fe6a48787fb66c2b260a
Shandea <86807815+Shandea@users.noreply.github.com>
2021-08-02 18:45:22 -0700

    initial setup

---
+++ ramptutoril/src/base.js
@@ -0,0 +1,13 @@
+import  firebase from "firebase/app";
+import "firebase/auth";
+
+const app = firebase.initializeApp({
+    apiKey: "AIzaSyCRdV1lpfC0Ql4a8n5tfJneNJeyRuy9AXE",
+    authDomain: "ramptutorial-51f66.firebaseapp.com",
+    projectId: "ramptutorial-51f66",
+    storageBucket: "ramptutorial-51f66.appspot.com",
+    messagingSenderId: "826780330540",
+    appId: "1:826780330540:web:4cc55024a45227a98dff89",
+    measurementId: "G-70GYSQMMCZ"
+  });
+export default app;
