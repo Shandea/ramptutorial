6047f97e776a026d9f651d15f47cdc9e3ee30b3f
Shandea <86807815+Shandea@users.noreply.github.com>
2021-08-03 11:43:45 -0700

    Revert "updated api"
    This reverts commit 0f88fedf621a131066e1cbbd07df2308441b6ca8.

--- ramptutoril/src/components/Login.js
+++ ramptutoril/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useCallback, useContext } from "react";
 import { withRouter, Redirect } from "react-router";
-import app from "../base";
-import { AuthContext } from "../Auth";
+import app from "../base.js";
+import { AuthContext } from "../Auth.js";
 
 const Login = ({ history }) => {
   const handleLogin = useCallback(
